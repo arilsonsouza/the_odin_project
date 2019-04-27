2dec9793e6c233d18a7f2593d4726e6633a1ac29
Arilson Souza <arilsons61@gmail.com>
2019-04-26 22:52:14 -0300

    event manager

---
+++ ruby/project_event_manager/lib/event_manager.rb
@@ -0,0 +1,51 @@
+require 'csv'
+require 'erb'
+require 'google/apis/civicinfo_v2'
+
+puts 'EventManager Initialized!'
+
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  
+  begin
+    legislators = civic_info.representative_info_by_address(
+                              address: zipcode,
+                              levels: 'country',
+                              roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists? "output"
+  
+  file_name = "output/tanks_#{id}.html"
+  
+  File.open(file_name, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+template_letter = File.read 'form_letter.erb'
+erb_template = ERB.new(template_letter)
+
+content = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+content.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id, form_letter)
+end
